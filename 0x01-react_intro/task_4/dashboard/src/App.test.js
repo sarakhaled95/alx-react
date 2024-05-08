1859e8da6bd13b74a8e4381b6644ee592777335d
sarakhaled95 <architectsarakhaled@outlook.com>
2024-05-08 14:01:57 +0300

    task4

---
+++ 0x01-react_intro/task_4/dashboard/src/App.test.js
@@ -0,0 +1,25 @@
+import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+
+describe("App Componeent Tests", () => {
+    it("Renders without crashing", () => {
+        const app = shallow(<App />);
+        expect(app).toBeDefined();
+    });
+    it("renders a div with the class App-header", () => {
+        const app = shallow(<App />);
+
+        expect(app.find(".App-header")).toBeDefined();
+    });
+    it("renders a div with the class App-body", () => {
+        const app = shallow(<App />);
+
+        expect(app.find(".App-body")).toBeDefined();
+    });
+    it("renders a div with the class App-footer", () => {
+        const app = shallow(<App />);
+
+        expect(app.find(".App-footer")).toBeDefined();
+    });
+});
