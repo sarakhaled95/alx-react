aec9f94ef26f41a3176618b363e524464d0b505a
sarakhaled95 <architectsarakhaled@outlook.com>
2024-05-15 15:22:41 +0300

    1

--- 0x03-React_component/task_0/dashboard/src/App/App.test.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.test.js
@@ -1,41 +1,70 @@
-import React from "react";
-import App from "./App";
-import Login from "../Login/Login";
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
-import Notifications from "../Notifications/Notifications";
-import { shallow } from "enzyme";
+import App from './App';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Login from '../Login/Login';
+import CourseList from '../CourseList/CourseList';
+import { shallow, render } from 'enzyme';
+import React from 'react'
 
-describe("App tests", () => {
-    it("renders without crashing", () => {
-        const component = shallow(<App />);
 
-        expect(component).toBeDefined();
+describe('App', () => {
+
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow(<App />)
+    })
+    test('App should not rash', () => {
+        expect(wrapper.length).toBe(1)
     });
-    it("should render Notifications component", () => {
-        const component = shallow(<App />);
 
-        expect(component.contains(<Notifications />)).toBe(true);
+    test("App renders a div with the class: App-header", () => {
+        wrapper.setProps({ isLoggedIn: true })
+        // console.debug(wrapper.html())
+        expect(wrapper.find('App-header').length).toBe(0)
     });
-    it("should render Header component", () => {
-        const component = shallow(<App />);
 
-        expect(component.contains(<Header />)).toBe(true);
+    test("App renders a div with the class: App-body", () => {
+        wrapper.setProps({ isLoggedIn: true })
+        expect(wrapper.find('.App-body').length).toBe(1);
     });
-    it("should render Login Component", () => {
-        const component = shallow(<App />);
 
-        expect(component.contains(<Login />)).toBe(true);
+    test("App renders a div with the class: App-footer", () => {
+        wrapper.setProps({ isLoggedIn: true })
+        expect(wrapper.find('.App-footer').length).toBe(1);
     });
-    it("should render Footer component", () => {
-        const component = shallow(<App />);
 
-        expect(component.contains(<Footer />)).toBe(true);
-    });
-    it("renders courselist if logged in", () => {
-        const component = shallow(<App isLoggedIn={true} />);
+    // test("check if App component contain the Notifications component", () => {
+    //     wrapper.setProps({isLoggedIn: true})
+    //     expect(wrapper.find(<Notifications />)).toBe(1)
+    // })
+    test("check if App component contain the Header component", () => {
+        wrapper.setProps({ isLoggedIn: true })
+        expect(wrapper.contains(<Header />)).toBeTruthy()
+    })
+    test("check if App component contain the Login component", () => {
+        wrapper.setProps({ isLoggedIn: false })
+        expect(wrapper.contains(<Login />)).toBeTruthy()
+    })
+    test("check if App component contain the Footer component", () => {
+        wrapper.setProps({ isLoggedIn: true })
+        expect(wrapper.contains(<Footer />)).toBeTruthy()
+    })
+    test("check that CourseList is not displayed ", () => {
+        wrapper.setProps({ isLoggedIn: false })
+        expect(wrapper.contains(<CourseList />)).toBeFalsy()
+    })
 
-        expect(component.contains(<CourseList />)).toBe(true);
-        expect(component.contains(<Login />)).toBe(false);
-    });
+    describe("when isLoggedIn prop is true", () => {
+        let wrapper;
+        beforeEach(() => {
+            wrapper = shallow(<App isLoggedIn={true} />)
+        })
+        test("verify that the Login component is not included", () => {
+            expect(wrapper.contains(<Login />)).toBeFalsy()
+        })
+        test("verify that the CourseList component is included", () => {
+            expect(wrapper.find('CourseList').length).toBe(1)
+        })
+    })
 });
